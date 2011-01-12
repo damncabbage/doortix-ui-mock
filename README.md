3e4ef2f85060155f5622a9a68487d35e761e4ccb
Rob Howard <damncabbage@gmail.com>
2011-01-13 00:14:34 +1100

    SquidTix --> DoorTix. Added the beginnings of mock v3.

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
-SquidTix
+DoorTix
 ========
 
 Demo URLs:
 
-* [v1](http://dev.robhoward.id.au/squidtix/mocks/v1/process.php)
-* [v2](http://dev.robhoward.id.au/squidtix/mocks/v2/process.php)
+* [v1](http://dev.robhoward.id.au/doortix/mocks/v1/process.php)
+* [v2](http://dev.robhoward.id.au/doortix/mocks/v2/process.php)
+* [v3](http://dev.robhoward.id.au/doortix/mocks/v3/process.php)
 
-(The name is just a placeholder, but it's enough to inspire a swirly blue theme. :) )
+The name is just a placeholder, but it's enough to inspire a slab-serif title font. :)
+
+(Formerly SquidTix, hence the name all over v1 and v2.)
---
+++ mocks/v3/process.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>DoorTix | Guest Check-In</title>
+	<link rel="stylesheet" href="css/screen.css" />
+</head>
+<body class="admin process">
+
+
+	<div id="header">
+		<div class="container">
+			<h1>DoorTix</h1>
+			<ul class="account">
+				<li class="first">Hey there, <span class="user">Samantha</span></li>
+				<li><a href="#">Logout</a></li>
+			</ul>
+
+			<ul class="navigation">
+				<li class="first selected"><a href="process.php">Guest Check-In</a></li>
+				<li><a href="status.php">How Are We Doing?</a></li>
+			</ul>
+		</div>
+	</div>
+
+	<div id="content">
+		<div class="container">
+		
+			<div id="search" class="panel">
+				<form action="process.html" method="post">
+
+					<span class="form">
+						<input name="terms" id="terms" type="text" class="text" />
+						<input name="submit" value="Search" type="submit" class="button" />
+					</span>
+					<div class="info">
+						<span class="action">Scan</span>
+						<span class="explanation">the barcode on an attendee's ticket</span>
+					</div>
+					<div class="info">
+						<span class="action">Search</span>
+						<span class="explanation">for an attendee by name, address or email.</span>
+					</div>
+
+				</form>
+			</div>
+
+		</div>
+	</div>
+
+	<div id="footer">
+		<div class="container">
+			<div class="status">
+				<span class="checked">33</span>
+				<span class="of">of</span>
+				<span class="total">2045</span>
+				<span class="description">attendees checked in</span>
+			</div>
+			<span class="legal">Brought you by <a href="http://robhoward.id.au">Rob Howard</a>, notepad scribblings and half a pack of Oreos.</span>
+		</div>
+	</div>
+
+</body>
+</html>
